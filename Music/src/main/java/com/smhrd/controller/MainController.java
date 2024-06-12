3dace71c31803301dbc20227f4f66446329525f8
smart <smart@smhrd>
2024-06-12 11:15:06 +0900

    다시 백

--- Music/src/main/java/com/smhrd/controller/MainController.java
+++ Music/src/main/java/com/smhrd/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.smhrd.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -11,19 +13,40 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.smhrd.Mapper.MemberMapper;
 import com.smhrd.entity.Member;
+import com.smhrd.entity.NewsVO;
+import com.smhrd.entity.Pagination;
 
 @Controller
 public class MainController {
 
 	@Autowired
-	private MemberMapper mmaper;
-
+	MemberMapper mmaper;
+	
 	@RequestMapping("mainList")
-	public ModelAndView main(HttpServletRequest request, @RequestParam(value = "page", defaultValue = "1") int page,
-			HttpSession session) throws Exception {
-
-		ModelAndView mav = new ModelAndView("/main");
-
+	public ModelAndView mainList(
+				HttpServletRequest request,
+				@RequestParam(value = "page", defaultValue = "1" ) int page,
+				@RequestParam(value = "targetUrl", defaultValue = "" ) String targetUrl,
+				HttpSession session
+			) throws Exception{
+		String targetPath = "main";
+		//로그인 정보 확인
+		if ( session.getAttribute("loginId") != null  && !session.getAttribute("loginId").equals("") ) {
+			//로그인 아이디 정보로 회원 정보 조회
+			Member loginMember = mmaper.memberLoginWithId( String.valueOf( session.getAttribute("loginId") ) );
+			if( loginMember == null ) {
+				targetPath = "login";
+			} 
+		} else {
+			//로그인 안 했을 시 할 행동 -> 로그인 페이지로 다시 이동시킬건지, 메인 페이지에서 다시 시작 시킬건지 
+			targetPath = "Login/login";
+		}
+		ModelAndView mav = new ModelAndView( targetPath ); 
+		
+		
+		
+		mav.addObject("targetUrl", !targetUrl.equals("") ? "targetUrl=" + targetUrl : "" );
 		return mav;
 	}
+
 }
